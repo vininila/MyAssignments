261cb952ac9aa0b1f5f81bc1bae2364bdb183fd4
vininila <110726339+vininila@users.noreply.github.com>
2022-08-22 06:27:56 -0700

    week3day2

---
+++ Week3Day2AbstractandInterface/ChangeOddIndexToUpperCase.java
@@ -0,0 +1,20 @@
+ package Week3Day2AbstractandInterface;
+
+public class ChangeOddIndexToUpperCase {
+
+	public static void main(String[] args) {
+		String test = "changeme";
+		char[] charArray=test.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			if(i%2==1)
+				charArray[i]=Character.toUpperCase(charArray[i]);
+				
+			System.out.print(charArray[i]);
+			
+			
+			
+		
+
+	}
+
+}}
